e102010de0b73ee0582a31fd69687c8dae24b5fc
Prakash Patil <prakash.patil@questionpro.com>
2019-10-04 15:09:29 +0530

    testing infinite loop

---
+++ HelloWord/src/com/qp/Hi.java
@@ -0,0 +1,19 @@
+package com.qp;
+
+public class Hi {
+	
+	private String name;
+	
+	public static final double GRAVITATIONAL_CONSTANT = 9.8;
+	
+	public static void main(String[] args) {
+		
+		for(;;) {
+			System.out.println("Nilesh");
+		}
+
+		
+		
+	}
+	
+}
